cd0768a5cf846f0a8a64664289df276a711d404c
Germain Pereira <pereira.germain@outlook.com>
2021-02-24 08:03:58 -0300

    Created handler for file

--- lib/rocketpay/numbers.ex
+++ lib/rocketpay/numbers.ex
@@ -7,7 +7,17 @@ defmodule Rocketpay.Numbers do
     # on terminal - interative elixir:
     # iex -S mix (iex)
     # Rocketpay.Numbers.sum_from_file("numbers")
-    file= File.read("#{filename}.csv")
+    # file= File.read("#{filename}.csv")
+
+    # = is also match operator (for polinomic operators)
+    # [a, b, c, d] = [1, 2, 3, 4]
+    # but ERROR: [a, b] = [1, 2, 3, 4]
+    file = File.read("#{filename}.csv")
+    handle_file(file)
   end
+
+  #defp = defining private functions
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 
 end
